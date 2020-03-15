be538a3c91a53181e2bb83d475b84164ac6b4c22
scarecrow <scarecrow2003@gmail.com>
2020-03-15 12:08:30 +0800

    commit

--- zynq/hls/mmult_fixed/mmult_fixed.cpp
+++ zynq/hls/mmult_fixed/mmult_fixed.cpp
@@ -73,6 +73,7 @@ void mmult_hw (AXI_VAL in_stream[IS_SIZE], AXI_VAL out_stream[OS_SIZE])
 		L1: for (int i = 0; i < TILING; i++) {
 			// Iterate over output classes
 			L2: for (int j = 0; j < CLASSES; j++) {
+#pragma HLS PIPELINE II=1
 				// Perform the dot product
 				out_T tmp = offset_buf[j];
 				L3: for(int k = 0; k < FEAT; k++) {
@@ -86,6 +87,7 @@ void mmult_hw (AXI_VAL in_stream[IS_SIZE], AXI_VAL out_stream[OS_SIZE])
 		// Stream out output matrix
 		// CSE548 TODO
 		STORE_O_1: for (int i = 0; i < TILING; i++) {
+#pragma HLS PIPELINE II=1
 			STORE_O_2: for (int j = 0; j < CLASSES; j+=OUT_WIDTH_RATIO) {
 				axi_T packet = 0;
 				PACK_OUT: for (int w = 0; w < OUT_WIDTH_RATIO; w++) {
