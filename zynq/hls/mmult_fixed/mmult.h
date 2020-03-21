53a7b37e476933860d5b5006b636f31e9421f9f6
Zhihua Su <zhihua@codapayments.com>
2020-03-21 19:38:58 +0800

    commit

--- zynq/hls/mmult_fixed/mmult.h
+++ zynq/hls/mmult_fixed/mmult.h
@@ -37,7 +37,7 @@ typedef unsigned long long axi_T;
 
 // Input matrix tiling factor
 // CSE548: TODO
-#define TILING 512
+#define TILING 1024
 
 // Input/Output Stream Size
 #define IS_SIZE ((CLASSES+OUT_WIDTH_RATIO-1)/OUT_WIDTH_RATIO+CLASSES*FEAT/W_WIDTH_RATIO+BATCH*FEAT/IN_WIDTH_RATIO)
